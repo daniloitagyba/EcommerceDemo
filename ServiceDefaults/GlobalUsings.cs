82c5acb6c6c19a789194007eefea3052a08039a9
Danilo Itagyba <daniloitagyba@gmail.com>
2024-10-08 08:45:23 -0300

    Add package references and framework reference to ServiceDefaults project Add package references and global usings to 'ServiceDefaults' project

---
+++ ServiceDefaults/GlobalUsings.cs
@@ -0,0 +1,27 @@
+﻿// Global using directives
+
+global using System.Net.Http.Headers;
+global using System.Security.Claims;
+global using System.Text;
+global using Asp.Versioning;
+global using Asp.Versioning.ApiExplorer;
+global using Microsoft.AspNetCore.Authentication;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.OpenApi;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.DependencyInjection.Extensions;
+global using Microsoft.Extensions.Diagnostics.HealthChecks;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Primitives;
+global using Microsoft.IdentityModel.JsonWebTokens;
+global using Microsoft.OpenApi.Any;
+global using Microsoft.OpenApi.Models;
+global using OpenTelemetry.Logs;
+global using OpenTelemetry.Metrics;
+global using OpenTelemetry.Trace;
+global using Scalar.AspNetCore;
