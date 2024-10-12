4dffe8f5dacd3b7359618e0d8511013c7ebd0fb3
Danilo Itagyba <daniloitagyba@gmail.com>
2024-10-12 08:03:39 -0300

    Add RabbitMQ event bus implementation with OpenTelemetry support in EventBusRabbitMQ project

---
+++ EventBusRabbitMQ/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿// Global using directives
+
+global using System.Diagnostics;
+global using System.Diagnostics.CodeAnalysis;
+global using System.Net.Sockets;
+global using System.Text;
+global using OpenTelemetry.Context.Propagation;
