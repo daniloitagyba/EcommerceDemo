7970fe3dd48390875d0810917b78da2d6817e35b
Danilo Itagyba <daniloitagyba@gmail.com>
2024-10-12 08:28:35 -0300

    Add ReverseProxy project with YARP configuration and health checks

---
+++ ReverseProxy/Extensions/Extensions.cs
@@ -0,0 +1,13 @@
+﻿namespace ReverseProxy.Extensions;
+
+public static class Extensions
+{
+    public static void AddApplicationServices(this IHostApplicationBuilder builder)
+    {
+        builder.Services.AddHttpForwarderWithServiceDiscovery();
+
+        builder.Services.AddHealthChecks()
+            .AddUrlGroup(new Uri("http://catalog-api/health"), name: "catalogapi-check")
+            .AddUrlGroup(new Uri("http://identity-api/health"), name: "identityapi-check");
+    }
+}
