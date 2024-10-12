eca42774ea8e890df9e97c2ff67475a06d94216d
Danilo Itagyba <daniloitagyba@gmail.com>
2024-10-12 08:03:54 -0300

    Add EventBus project with core abstractions and extensions for event handling

---
+++ EventBus/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿// Global using directives
+
+global using System.Diagnostics.CodeAnalysis;
+global using System.Text.Json;
+global using System.Text.Json.Serialization;
+global using System.Text.Json.Serialization.Metadata;
+global using EventBus.Abstractions;
+global using EventBus.Events;
+global using Microsoft.Extensions.DependencyInjection;
