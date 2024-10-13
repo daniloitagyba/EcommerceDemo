a51ed25f2524d0a08508de7881d63468a85835e0
Danilo Itagyba <daniloitagyba@gmail.com>
2024-10-13 16:44:56 -0300

    Enhance app configuration, styling, and functionality
    Significantly modified `Program.cs`:
    - Added `using` directives for various services and endpoints.
    - Initialized `WebApplication` builder with service defaults, Razor components, and application services.
    - Replaced `MapGet` with `MapDefaultEndpoints`.
    - Configured HTTP request pipeline with exception handling, HSTS, antiforgery, static files, Razor components, authentication, and webhook endpoints.
    - Removed "Hello World!" endpoint.
    
    Updated `launchSettings.json`:
    - Changed `$schema` URL to `http`.
    - Added IIS settings and updated `applicationUrl` for profiles.
    - Added `IIS Express` profile.
    
    Modified `Webhook.Client.csproj`:
    - Moved `MainLayout.razor.css` to `Content` item group.
    
    Updated `appsettings.json`:
    - Added `WebhookClientOptions` with `Token`.
    
    Extensively updated `app.css`:
    - Added styles for headings, paragraphs, form elements, validation messages, error boundaries, buttons, grid placeholders, text inputs, error messages, and `quickgrid` component.

--- Webhook.Client/Program.cs
+++ Webhook.Client/Program.cs
@@ -1,6 +1,36 @@
+using ServiceDefaults.Extensions;
+using Webhook.Client.Components;
+using Webhook.Client.Endpoints;
+using Webhook.Client.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+builder.AddApplicationServices();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapDefaultEndpoints();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseAntiforgery();
+
+app.UseStaticFiles();
+
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+app.MapAuthenticationEndpoints();
+
+app.MapWebhookEndpoints();
 
 app.Run();
