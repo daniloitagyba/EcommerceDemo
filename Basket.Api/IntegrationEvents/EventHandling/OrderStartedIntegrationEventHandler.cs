8e6131900dd64cd174f59db5718494488bbb1ab6
Danilo Itagyba <daniloitagyba@gmail.com>
2024-10-13 12:16:36 -0300

    Add OrderStartedIntegrationEventHandler to handle events

---
+++ Basket.Api/IntegrationEvents/EventHandling/OrderStartedIntegrationEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using Basket.Api.IntegrationEvents.Events;
+using Basket.Api.Repositories;
+using EventBus.Abstractions;
+
+namespace Basket.Api.IntegrationEvents.EventHandling;
+
+public class OrderStartedIntegrationEventHandler(
+    IBasketRepository repository,
+    ILogger<OrderStartedIntegrationEventHandler> logger) : IIntegrationEventHandler<OrderStartedIntegrationEvent>
+{
+    public async Task Handle(OrderStartedIntegrationEvent @event)
+    {
+        logger.LogInformation("Handling integration event: {IntegrationEventId} - ({@IntegrationEvent})", @event.Id, @event);
+
+        await repository.DeleteBasketAsync(@event.UserId);
+    }
+}
