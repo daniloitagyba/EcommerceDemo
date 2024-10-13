a897e2d6c5c263dbe4a037a8b2e00c019c489f3d
Danilo Itagyba <daniloitagyba@gmail.com>
2024-10-13 11:35:46 -0300

    Add extension method for configuring app services

---
+++ Basket.Api/Extensions/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
+using Basket.Api.IntegrationEvents.EventHandling;
+using Basket.Api.IntegrationEvents.Events;
+using Basket.Api.Repositories;
+using EventBus.Extensions;
+using EventBusRabbitMQ;
+using ServiceDefaults.Extensions;
+
+namespace Basket.Api.Extensions;
+
+public static class Extensions
+{
+    public static void AddApplicationServices(this IHostApplicationBuilder builder)
+    {
+        builder.AddDefaultAuthentication();
+
+        builder.AddRedisClient("redis");
+
+        builder.Services.AddSingleton<IBasketRepository, RedisBasketRepository>();
+
+        builder.AddRabbitMqEventBus("eventbus")
+            .AddSubscription<OrderStartedIntegrationEvent, OrderStartedIntegrationEventHandler>();
+    }
+}
