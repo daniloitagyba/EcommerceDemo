c0f822d64068659ab885c531ae2d09cfcd17458a
Danilo Itagyba <daniloitagyba@gmail.com>
2024-10-13 14:00:00 -0300

    Add WebhooksContext class for managing webhook subscriptions

---
+++ Webhook.Api/Infrastructure/WebhooksContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Webhook.Api.Model;
+
+namespace Webhook.Api.Infrastructure
+{
+    /// <remarks>
+    /// Add migrations using the following command inside the 'Webhooks.API' project directory:
+    ///
+    /// dotnet ef migrations add [migration-name]
+    /// </remarks>
+    public class WebhooksContext(DbContextOptions<WebhooksContext> options) : DbContext(options)
+    {
+        public DbSet<WebhookSubscription> Subscriptions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WebhookSubscription>(eb =>
+            {
+                eb.HasIndex(s => s.UserId);
+                eb.HasIndex(s => s.Type);
+            });
+        }
+    }
+}
