1079db1d4a89b34b7d485e7272336b49d229addc
Danilo Itagyba <daniloitagyba@gmail.com>
2024-10-13 13:59:45 -0300

    Add extension methods for tracing, services, and validation
    Enhance the `Webhook.Api` project with new extension methods:
    - `ActivityExtensions` for setting exception tags on `Activity` objects.
    - `Extensions` for adding application services and event bus subscriptions.
    - `MigrateDbContextExtensions` for handling database migrations with tracing.
    - `RouteHandlerBuilderExtensions` for validating webhook subscription requests.

---
+++ Webhook.Api/Extensions/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using EventBus.Extensions;
+using EventBusRabbitMQ;
+using ServiceDefaults.Extensions;
+
+
+namespace Webhook.Api.Extensions
+{
+    internal static class Extensions
+    {
+        public static void AddApplicationServices(this IHostApplicationBuilder builder)
+        {
+            builder.AddDefaultAuthentication();
+
+            builder.AddRabbitMqEventBus("eventbus")
+                   .AddEventBusSubscriptions();
+
+            builder.AddNpgsqlDbContext<WebhooksContext>("webhooksdb");
+
+            builder.Services.AddMigration<WebhooksContext>();
+
+            builder.Services.AddTransient<IGrantUrlTesterService, GrantUrlTesterService>();
+            builder.Services.AddTransient<IWebhooksRetriever, WebhooksRetriever>();
+            builder.Services.AddTransient<IWebhooksSender, WebhooksSender>();
+        }
+
+        private static void AddEventBusSubscriptions(this IEventBusBuilder eventBus)
+        {
+            eventBus.AddSubscription<ProductPriceChangedIntegrationEvent, ProductPriceChangedIntegrationEventHandler>();
+            eventBus.AddSubscription<OrderStatusChangedToShippedIntegrationEvent, OrderStatusChangedToShippedIntegrationEventHandler>();
+            eventBus.AddSubscription<OrderStatusChangedToPaidIntegrationEvent, OrderStatusChangedToPaidIntegrationEventHandler>();
+        }
+    }
+}
