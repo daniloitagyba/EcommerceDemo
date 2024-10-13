f0524179c1819750b315db234ef8480188763d05
Danilo Itagyba <daniloitagyba@gmail.com>
2024-10-13 11:35:12 -0300

    Add IBasketRepository and RedisBasketRepository
    Introduce IBasketRepository interface with methods for managing customer baskets. Implement RedisBasketRepository to handle basket operations using Redis, including methods for getting, updating, and deleting baskets.

---
+++ Basket.Api/Repositories/IBasketRepository.cs
@@ -0,0 +1,10 @@
+﻿using Basket.Api.Model;
+
+namespace Basket.Api.Repositories;
+
+public interface IBasketRepository
+{
+    Task<CustomerBasket?> GetBasketAsync(string customerId);
+    Task<CustomerBasket?> UpdateBasketAsync(CustomerBasket basket);
+    Task<bool> DeleteBasketAsync(string id);
+}
---
+++ Basket.Api/Repositories/RedisBasketRepository.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Basket.Api.Model;
+using Microsoft.AspNetCore.Http.Json;
+using StackExchange.Redis;
+
+namespace Basket.Api.Repositories;
+
+public class RedisBasketRepository(ILogger<RedisBasketRepository> logger, IConnectionMultiplexer redis) : IBasketRepository
+{
+    private readonly IDatabase _database = redis.GetDatabase();
+
+    // implementation:
+
+    // - /basket/{id} "string" per unique basket
+    private static RedisKey BasketKeyPrefix = "/basket/"u8.ToArray();
+    // note on UTF8 here: library limitation (to be fixed) - prefixes are more efficient as blobs
+
+    private static RedisKey GetBasketKey(string userId) => BasketKeyPrefix.Append(userId);
+
+    public async Task<bool> DeleteBasketAsync(string id)
+    {
+        return await _database.KeyDeleteAsync(GetBasketKey(id));
+    }
+
+    public async Task<CustomerBasket?> GetBasketAsync(string customerId)
+    {
+        using var data = await _database.StringGetLeaseAsync(GetBasketKey(customerId));
+
+        if (data is null || data.Length == 0)
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<CustomerBasket>(data.Span, new JsonSerializerOptions {
+            PropertyNameCaseInsensitive = true });
+    }
+
+    public async Task<CustomerBasket?> UpdateBasketAsync(CustomerBasket basket)
+    {
+        var json = JsonSerializer.SerializeToUtf8Bytes(basket, new JsonSerializerOptions {
+            PropertyNameCaseInsensitive = true });
+        var created = await _database.StringSetAsync(GetBasketKey(basket.BuyerId), json);
+
+        if (!created)
+        {
+            logger.LogInformation("Problem occurred persisting the item.");
+            return null;
+        }
+
+
+        logger.LogInformation("Basket item persisted successfully.");
+        return await GetBasketAsync(basket.BuyerId);
+    }
+}
