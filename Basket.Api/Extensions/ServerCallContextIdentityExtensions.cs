630a4b6d06cfb902ec04a68f64b96746e29d4841
Danilo Itagyba <daniloitagyba@gmail.com>
2024-10-13 11:36:22 -0300

    Add ServerCallContext extensions for user identity and name

---
+++ Basket.Api/Extensions/ServerCallContextIdentityExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+using Grpc.Core;
+
+namespace Basket.Api.Extensions;
+
+public static class ServerCallContextIdentityExtensions
+{
+    public static string? GetUserIdentity(this ServerCallContext context) => context.GetHttpContext().User.FindFirst("sub")?.Value;
+    public static string? GetUserName(this ServerCallContext context) => context.GetHttpContext().User.FindFirst(x => x.Type == ClaimTypes.Name)?.Value;
+}
